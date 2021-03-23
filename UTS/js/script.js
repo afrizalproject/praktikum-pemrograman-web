a3e4a3d49dcd05db60963ffc2e68496f11660816
afrizalproject <afrizalrizky777@gmail.com>
2021-03-23 08:56:34 +0700

    uts done

---
+++ UTS/js/script.js
@@ -0,0 +1,30 @@
+function kalkulator() {
+
+    // Muhammad Afrizal Rizky Widyanto (19650048)
+    // Kelas: Pemrograman Web F
+
+    //variable angka satu
+    var angkaSatu = parseInt(document.getElementById("angkaSatu").value);
+
+    //variable angka dua
+    var angkaDua = parseInt(document.getElementById("angkaDua").value);
+
+    //variable operasi aritmatik
+    var op = document.getElementById("op").value;
+
+    //variable angka hasil
+    var hasil = document.getElementById("hasil");
+
+    //variable untuk menampung total angka satu dan angka dua
+    var total;
+
+
+    var proses = angkaSatu / angkaDua;
+    if (angkaSatu % angkaDua == 0) {
+        total = proses;
+    } else {
+        total = (angkaSatu % angkaDua);
+    }
+
+    document.calculator.hasil.value = total;
+}
